ed485e19165a6435fd2874b036a537d136d144c9
VPurcell99 <vpurcell290@gmail.com>
2021-05-26 16:24:31 -0400

    Finished HW2

--- HW2-TwitterApi/twitterApi.py
+++ HW2-TwitterApi/twitterApi.py
@@ -1,11 +1,14 @@
+# Vincent Purcell
+# Data Mining 1 - Summer 2021 - HW2
+# Some of the code was taken and modified from Professor Breitzman's code
+
 import json
 
 import tweepy # conda friendly. Most starts of any tweeter API project on Github
 from IPython.core.display import HTML
 from IPython.display import display
 import config
-
-# Code below is Professor Breitzman's code modified to use my personal api key.
+from datetime import datetime
 
 consumer_key = config.apiKey
 consumer_secret = config.apiSecretKey
@@ -25,4 +28,14 @@ def get_tweets_from_search_term(api, query, number_of_tweets=100):
     for count, tweet in enumerate(tweepy.Cursor(api.search, q=query, lang='en', tweet_mode='extended').items(number_of_tweets), 1):
         display(HTML('{}. ({} by {}) {}'.format(count, tweet.created_at, tweet.author.screen_name, tweet.full_text)))
 
-get_tweets_from_twitterhandle(api, 'Eagles', )
+def get_followers_from_twitterhandle(api, twitter_handle, amount=20):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    display(HTML('{} Most Recent Followers of {} as of {}'.format(amount, twitter_handle, dt_string)))
+    display(HTML('--------------------------------------'))
+    for count, follower in enumerate(tweepy.Cursor(api.followers, screen_name=twitter_handle).items(amount)):
+        display(HTML('{}. {}'.format(count+1, follower.screen_name)))
+
+get_tweets_from_twitterhandle(api, 'Eagles', 10)
+
+get_followers_from_twitterhandle(api, 'NFL', 15)
